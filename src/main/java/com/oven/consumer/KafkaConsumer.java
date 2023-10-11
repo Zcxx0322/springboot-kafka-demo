50a22632defc9e12e51eb6a53fb8f84fd523e71a
Zcx <zhangchongxin@cdgeekcamp.com>
2023-10-11 19:39:16 +0800

    删除多余代码

--- src/main/java/com/oven/consumer/KafkaConsumer.java
+++ src/main/java/com/oven/consumer/KafkaConsumer.java
@@ -1,15 +1,11 @@
 package com.oven.consumer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @SuppressWarnings("unused")
 @Component
 public class KafkaConsumer {
-
-    @Value("${kafka.consumer.topic}")
-    private String consumerTopic;
 
     @KafkaListener(topics = "${kafka.consumer.topic}")
     public void consumer(String message) {
